400987d82d12e169955ed7e9fb3afda8a03854ed
eemilyy <emilyalmeeidas@gmail.com>
2022-12-09 19:14:02 -0300

    alterando o arquivo server

---
+++ src/app.ts
@@ -0,0 +1,5 @@
+import "reflect-metadata"; 
+import express from "express";
+import "./database";
+
+export const app = express();
--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import "reflect-metadata"; 
-import express from "express";
-import "./database";
-
-const app = express();
-
-app.use(express.json());
-
-app.listen(3000, () => console.log("Server is running"));
